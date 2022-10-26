55250ebefbdff1a5cc9e27f716a9fd2eda2069f4
agnes0109 <62559359+agnes0109@users.noreply.github.com>
2022-10-26 17:48:32 +0200

    Update script.js

--- script.js
+++ script.js
@@ -94,7 +94,7 @@ function updateClock() {
 
   document.getElementById('clock').textContent = time;
 
-  const text = mode === 'pomodoro' ? '¡A estudiar!' : '¡Descanso!';
+  const text = mode === 'pomodoro' ? 'STUDY TIME' : 'BREAK TIME';
   document.title = `${time} - ${text}`;
   document.getElementById('text').textContent = text;
 
